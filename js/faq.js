47beb1e64caff150c1ec50ddb8158f27874e48fb
BilyakNatalya <nataliabiliak18@gmail.com>
2022-11-07 18:29:53 +0200

    Added faq.js

--- js/faq.js
+++ js/faq.js
@@ -1,15 +1,40 @@
-const items = document.querySelectorAll('.accordion button');
+(function(){
 
-function toggleAccordion() {
-  const itemToggle = this.getAttribute('aria-expanded');
+  const questionsList = [
+      {
+          header: "Does this tour require physical fitness?",
+          answer: "This tour is suitable for untrained tourists. No cliff climbing, we promise."
+      },
+      {
+          header: "Will I be able to see the Northern Lights?",
+          answer: "We cannot promise you this. The Northern Lights only appear in winter under special circumstances. But we will hunt it in Tromsø."
+      },
+      {
+          header: "Can I take my children with me on the tour?",
+          answer: "Yes, we offer a 50% discount for children."
+      },
+      {
+          header: "Can I join you not in Oslo, but in another city?",
+          answer: "Depending on the weather conditions, our route may change, so we strongly recommend starting with other tourists from Oslo. We do not change the route to pick up tourists on the way."
+      }
+  ];
 
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
+  function render(){
+      const questions = document.querySelector('.questions__list');
+      for (const question of questionsList){
+          questions.innerHTML += `
+              <div class="questions__item">
+                  <button class="questions__header">${question.header}</button>
+                  <span class="questions__answer">${question.answer}</span>
+              </div>
+          `; 
+      }
+      const questionHeaders = document.querySelectorAll('.questions__header');
+      for (const questionHeader of questionHeaders){
+          questionHeader.addEventListener('click', ({ target }) => {
+              target.classList.toggle('active');
+          });
+      }
   }
-
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
-  }
-}
-
-items.forEach((item) => item.addEventListener('click', toggleAccordion));
+  render();
+})();
