5efb8a6c8696986dbeb3f0d00ce36e97ae4dbfb4
irinaborisenko <irinaborisenko2019@gmail.com>
2022-11-07 12:31:59 +0000

    added functions for carousel Teestimonials

--- js/testimonials.js
+++ js/testimonials.js
@@ -25,18 +25,23 @@
             dataTestimonials: "January 12, 2022"
         },
     ];
-
     let currentSlideTestimonialIdx = 0;
     function renderSlideTestimonials() {
-        const slideContainer = document.querySelector('.testimonials__carousel-slide');
-        slideContainer.innerHTML = slides[currentSlideTestimonialIdx];
+        const slideContainerTestimonials = document.querySelector('.testimonials__carousel-slide');
+        slideContainerTestimonials.innerHTML = `
+        <article class="carousel__person">
+                                <img src="${testimonials[currentSlideTestimonialIdx].imageTestimonials}" class="carousel__person-img" alt="${testimonials[currentSlideTestimonialIdx].imageNameTestimonials}">
+                        <h4 class="carousel__person">${testimonials[currentSlideTestimonialIdx].nameTestimonials}</h4>
+                        <p class="carousel__person-text">${testimonials[currentSlideTestimonialIdx].textTestimonials}</p>
+                      <p class="carousel__person-data">${testimonials[currentSlideTestimonialIdx].dataTestimonials}</p>
+                 </article>`;
     }
     function nextSlideTestimonials() {
-        currentSlideTestimonialIdx = currentSlideTestimonialIdx + 1 >= slides.length ? 0 : currentSlideTestimonialIdx + 1;
+        currentSlideTestimonialIdx = currentSlideTestimonialIdx + 1 >= testimonials.length ? 0 : currentSlideTestimonialIdx + 1;
         renderSlideTestimonials();
     }
     function prevSlideTestimonials() {
-        currentSlideTestimonialIdx = currentSlideTestimonialIdx - 1 < 0 ? slides.length - 1 : currentSlideTestimonialIdx - 1;
+        currentSlideTestimonialIdx = currentSlideTestimonialIdx - 1 < 0 ? testimonials.length - 1 : currentSlideTestimonialIdx - 1;
         renderSlideTestimonials();
     }
     renderSlideTestimonials();
