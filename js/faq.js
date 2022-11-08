5b32868dde2fd8484f27ddb6d8b6ba4a0b5fa9c1
BilyakNatalya <nataliabiliak18@gmail.com>
2022-11-08 12:26:49 +0200

    Added new accordion html, css,js

--- js/faq.js
+++ js/faq.js
@@ -1,29 +1,16 @@
-$(document).ready(function(){
-    $(".set > a").on("click", function(){
-      if($(this).hasClass('active')){
-        $(this).removeClass("active");
-        $(this).siblings('.content').slideUp(200);
-        $(".set > a span").removeClass("minus").addClass("plus");
-      }else{
-        $(".set > a span").removeClass("minus").addClass("plus");
-        $(this).find("span").removeClass("plus").addClass("minus");
-        $(".set > a").removeClass("active");
-        $(this).addClass("active");
-        $('.content').slideUp(200);
-        $(this).siblings('.content').slideDown(200);
-      }    
-    });
+$(function() {
+  $('.acc_ctrl').on('click', function(e) {
+    e.preventDefault();
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+      $(this).next()
+      .stop()
+      .slideUp(300);
+    } else {
+      $(this).addClass('active');
+      $(this).next()
+      .stop()
+      .slideDown(300);
+    }
   });
-  
-  // var parentTitleAcc = document.querySelector('.sn-accordion-container');
-  // var titleAcc = document.querySelector('.sn-accordion-container a');
-  
-  // titleAcc.onclick = function (e) {
-  // 	e.preventDefault();
-  //   var target = e.target;
-    
-  //   if (traget.titleAcc != 'a') return;
-    
-  //   highlight(target); // подсветить TD
-  // };
-  
+});
